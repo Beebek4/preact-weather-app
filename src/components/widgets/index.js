29eb77896d440b69cbe6d9331250fd5957660fd7
Caleb Lim <c.limjiale@se21.qmul.ac.uk>
2023-03-20 16:57:21 +0000

    Revert "Revert "Merge branch 'main' of https://github.com/Beebek4/preact-weather-app""
    This reverts commit 8466ea7a7cebe0ec596154459172451c6b2ce8cc.

--- src/components/widgets/index.js
+++ src/components/widgets/index.js
@@ -1,14 +1,27 @@
 // import preact
 import { h, render, Component } from 'preact';
-	
+import style from '../widgets/iphone_widgets';	
+
 export default class Widgets extends Component {
 
 	// rendering a function when the button is clicked
 	render() {
 		return (
-			<div>
-				<button onClick={cFunction}>
-					Display Weather
+			<div class={style.container}>
+				<button class={ style.uv } clickFunction={ this.fetchWeatherData }>
+					UV Index
+				</button>
+				<button class={ style.button } clickFunction={ this.fetchWeatherData }>
+					Gust Speed
+				</button>
+				<button class={ style.button } clickFunction={ this.fetchWeatherData }>
+					Precipitation
+				</button>
+				<button class={ style.button } clickFunction={ this.fetchWeatherData }>
+					Feels Like
+				</button>
+				<button class={ style.button } clickFunction={ this.fetchWeatherData }>
+					Visibility
 				</button>
 			</div>
 		);
