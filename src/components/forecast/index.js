ef8af9235cc3fdbcb16fb6365e402d24a20d7a57
Caleb Lim <cl@vnwlim.com>
2023-03-22 13:35:47 +0000

    Added the forcasting feature a little
    Using hardcoded values because I cba

--- src/components/forecast/index.js
+++ src/components/forecast/index.js
@@ -1,7 +1,39 @@
 // import preact
 import { h, render, Component } from 'preact';
 import style from '../forecast/iphone_forecast';
-	
+console.log("From forecast")
+var data = [
+	{
+		"day": "2023-03-22 15:00:00",
+		"icon": "10d",
+		"maxtemp": 14,
+		"mintemp": 7
+	},
+	{
+		"day": "2023-03-23 15:00:00",
+		"icon": "10d",
+		"maxtemp": 12,
+		"mintemp": 7
+	},
+	{
+		"day": "2023-03-24 15:00:00",
+		"icon": "10d",
+		"maxtemp": 12,
+		"mintemp": 8
+	},
+	{
+		"day": "2023-03-25 15:00:00",
+		"icon": "10d",
+		"maxtemp": 12,
+		"mintemp": 6
+	},
+	{
+		"day": "2023-03-26 15:00:00",
+		"icon": "10d",
+		"maxtemp": 7,
+		"mintemp": 1
+	}
+]
 export default class Forecast extends Component {
 
 	// rendering a function when the button is clicked
@@ -11,30 +43,25 @@ export default class Forecast extends Component {
 				<div class={style.daybox}>
 					<p>yae</p>
 				</div>
-				<div class={style.daybox}>
-					<p>yae</p>
-				</div>
-				<div class={style.daybox}>
-					<p>yae</p>
-				</div>
-				<div class={style.daybox}>
-					<p>yae</p>
-				</div>
-				<div class={style.daybox}>
-					<p>yae</p>
-				</div>
-				<div class={style.daybox}>
-					<p>yae</p>
-				</div>
-				<div class={style.daybox}>
-					<p>yae</p>
-				</div>
-				<div class={style.daybox}>
-					<p>yae</p>
-				</div>
-				<div class={style.daybox}>
-					<p>yae</p>
-				</div>
+				<div class={style.container}>
+					{data.map(({ day, icon, maxtemp, mintemp }) => {
+						const date = new Date(day);
+						const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+						const weatherIconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+						return (
+						<div class={style.daybox}>
+							<div>{dayOfWeek}</div>
+							<img src={weatherIconUrl} alt="" />
+							<div>
+							<span>Max: {maxtemp}°C</span>
+							<span>Min: {mintemp}°C</span>
+							</div>
+						</div>
+						);
+					})}
+					</div>
+				
 			</div>
 		);
 	}
