ffd0e8d18159d3ce9df02e749d5b22095e2a2a54
Caleb Lim <cl@vnwlim.com>
2023-03-23 22:35:09 +0000

    Hard code values till we get it to work otherwise

--- src/components/forecast/index.js
+++ src/components/forecast/index.js
@@ -3,12 +3,38 @@ import { h, render, Component } from 'preact';
 import style from '../forecast/iphone_forecast';
 // an empty array of objects for the 5 forecasted day of the week
 var weekData=[
-	{"day": "", "icon": "", "maxtemp": 0, "mintemp": 0},
-	{"day": "", "icon": "", "maxtemp": 0, "mintemp": 0},
-	{"day": "", "icon": "", "maxtemp": 0, "mintemp": 0},
-	{"day": "", "icon": "", "maxtemp": 0, "mintemp": 0},
-	{"day": "", "icon": "", "maxtemp": 0, "mintemp": 0},
+    {
+        "day": "2023-03-24 00:00:00",
+        "icon": "04n",
+        "maxtemp": 12,
+        "mintemp": 7
+    },
+    {
+        "day": "2023-03-25 00:00:00",
+        "icon": "10n",
+        "maxtemp": 12,
+        "mintemp": 7
+    },
+    {
+        "day": "2023-03-26 00:00:00",
+        "icon": "04n",
+        "maxtemp": 8,
+        "mintemp": 5
+    },
+    {
+        "day": "2023-03-27 00:00:00",
+        "icon": "04n",
+        "maxtemp": 8,
+        "mintemp": 3
+    },
+    {
+        "day": "2023-03-28 00:00:00",
+        "icon": "01n",
+        "maxtemp": 8,
+        "mintemp": 1
+    }
 ];
+
 export default class Forecast extends Component {
 
 	render(props) {
@@ -32,10 +58,11 @@ export default class Forecast extends Component {
 				<div class={style.container}>
 					{weekData.map(({ day, icon, maxtemp, mintemp }) => {
 						const weatherIconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-
+						const date = new Date(day);
+						const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
 						return (
 						<div class={style.daybox}>
-							<div class={style.day}>{day}</div>
+							<div class={style.day}>{dayOfWeek}</div>
 							<img src={weatherIconUrl} alt="" />
 							<div>
 							<span>H: {maxtemp}°C </span>
